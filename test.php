a92dd37f0951ee9e5b9fb5f366b881207281be83
Arvin de Leon <arvin@revdojo.com>
2026-06-11 22:53:38 +0800

    init

---
+++ test.php
@@ -0,0 +1,28 @@
+<?php
+
+return 2;
+
+$x = 1;
+$ex = "1";
+$y = 1;
+
+echo $x + $y;
+
+
+$one = $x;
+$one2 = $y;
+
+if ($one == $x) {
+    if ($one2 == $y) {
+        echo $x + $y;
+    }
+}
+
+?>
+
+<html>
+
+<body>
+test
+</body>
+</html>
